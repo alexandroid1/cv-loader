aec23734b3998923b8805305d35cb0fe5fa9cc5c
Oleksandr <avpavlov96@gmail.com>
2016-12-26 17:17:41 +0300

    new code

--- src/main/java/ua/alexandroid1/oleksandr/pages/CVLoader.java
+++ src/main/java/ua/alexandroid1/oleksandr/pages/CVLoader.java
@@ -6,6 +6,10 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Created by Oleksandr on 26.12.2016.
  */
@@ -15,25 +19,21 @@ public class CVLoader {
     private String searchUrl;
     private String searchKeyWord;
 
-    public CVLoader(WebDriver driver)
-    {
+    public CVLoader(WebDriver driver) {
         this.driver = driver;
     }
 
-    public CVLoader setSearchURL(String searchUrl)
-    {
+    public CVLoader setSearchURL(String searchUrl) {
         this.searchUrl = searchUrl;
         return this;
     }
 
-    public CVLoader setSearchKeyWord(String searchKeyWord)
-    {
+    public CVLoader setSearchKeyWord(String searchKeyWord) {
         this.searchKeyWord = searchKeyWord;
         return this;
     }
 
-    public void searchByKeyWord(int waitSeconds)
-    {
+    public void searchByKeyWord(int waitSeconds) {
         driver.get(searchUrl);
         WebDriverWait wait = new WebDriverWait(driver, waitSeconds);
 
@@ -44,5 +44,31 @@ public class CVLoader {
         WebElement searchButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@data-qa='navi-search__button']")));
         searchButton.click();
     }
+
+    public ArrayList<String> getCvIds() {
+        ArrayList<String> getProfileIds = new ArrayList<String>();
+        java.util.List<WebElement> links = driver.findElements(By.xpath("//a[contains(@data-qa,'vacancy-title')]"));
+
+        for (int i = 0; i < links.size(); i++) {
+            String profileUrl = links.get(i).getAttribute("href");
+            getProfileIds.add(getIDfromProfileUrl(profileUrl));
+            System.out.println(getIDfromProfileUrl(profileUrl));
+        }
+        return getProfileIds;
+    }
+
+    private String getIDfromProfileUrl(String profileUrl) {
+        Pattern p = Pattern.compile(".*?(\\d+)", Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
+        Matcher m = p.matcher(profileUrl);
+        if (m.find()) return m.group(1);
+        return null;
+    }
+
+    public void getNextPage(int waitSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, waitSeconds);
+        WebElement element = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@data-qa='pager-next']")));
+        element.click();
+    }
+
 
 }
