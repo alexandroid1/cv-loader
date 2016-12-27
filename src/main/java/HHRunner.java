dd9aa3acffba3d0ef35dea3382ab1f6d2a68aeba
Oleksandr <avpavlov96@gmail.com>
2016-12-27 17:10:43 +0300

    all values into properties

--- src/main/java/HHRunner.java
+++ src/main/java/HHRunner.java
@@ -4,18 +4,19 @@ import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.firefox.internal.ProfilesIni;
 import ua.alexandroid1.oleksandr.pages.CVLoader;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Properties;
 
 public class HHRunner {
 
     public static void main(String[] args) {
 
-        File f = new File("geckodriver.exe");
+        Properties prop = new Properties();
+        File f = new File(prop.getProperty("driverPathName"));
         System.setProperty("webdriver.gecko.driver", f.getAbsolutePath());
         ProfilesIni profile = new ProfilesIni();
-        FirefoxProfile myprofile = profile.getProfile("default");
+        FirefoxProfile myprofile = profile.getProfile(prop.getProperty("profileName"));
         WebDriver driver = new FirefoxDriver(myprofile);
 
         double randNumber = Math.random();
@@ -23,8 +24,8 @@ public class HHRunner {
 
         CVLoader cvLoader = new CVLoader(driver);
         cvLoader
-                .setSearchURL("http://www.hh.ru")
-                .setSearchKeyWord("java")
+                .setSearchURL(prop.getProperty("searchURL"))
+                .setSearchKeyWord(prop.getProperty("searchKeyWord"))
                 .searchByKeyWord(waitSeconds);
 
         while (true){
