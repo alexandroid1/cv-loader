0350e2f0c579ea87e013b6c6871db8b0b2a6f53f
Oleksandr <avpavlov96@gmail.com>
2016-12-29 20:18:26 +0300

    cover letter

--- src/main/java/ua/alexandroid1/oleksandr/pages/CVApplyer.java
+++ src/main/java/ua/alexandroid1/oleksandr/pages/CVApplyer.java
@@ -1,12 +1,16 @@
 package ua.alexandroid1.oleksandr.pages;
 
+import junit.framework.Assert;
+import org.hamcrest.core.Is;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.NoSuchElementException;
+import java.util.Set;
 
 /**
  * Created by Oleksandr on 29.12.2016.
@@ -22,72 +26,95 @@ public class CVApplyer {
         this.cvId = cvId;
     }
 
+    public boolean isElementPresent_1(WebDriver driver, By locator) {
+        try {
+            driver.findElement(locator);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
     public void applyForCV(int waitSeconds) {
         driver.get(url + cvId);
         System.out.println("");
         System.out.println(url + cvId);
 
-        // green button apply click
-        try {
-            WebDriverWait wait = new WebDriverWait(driver, waitSeconds);
-            WebElement applyLink = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@data-qa='vacancy-response-link']")));
-            applyLink.click();
-        } catch (NoSuchElementException ignored) {
-            System.out.print("-");
+        if (driver.findElements(By.xpath("//a[@data-qa='vacancy-response-link']")).size() > 0) {
+            // green button apply click
+            try {
+                WebElement applyLink = driver.findElement(By.xpath("//a[@data-qa='vacancy-response-link']"));
+                applyLink.click();
+            } catch (NoSuchElementException ignored) {
+                System.out.print("-");
+            }
+
+            if (driver.findElements(By.xpath("//span[@data-qa='vacancy-response-link-force']")).size() > 0) {
+                // apply from anywhere
+                try {
+                    WebDriverWait wait = new WebDriverWait(driver, waitSeconds);
+                    WebElement applyFromAnywhereLnk = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[@data-qa='vacancy-response-link-force']")));
+                    applyFromAnywhereLnk.click();
+                } catch (NoSuchElementException ignored) {
+                    System.out.print("-");
+                }
+
+
+                if (driver.findElements(By.xpath("//span[@class='link-switch-secondary']")).size() > 0) {
+                    // show text area
+                    try {
+                        WebDriverWait wait = new WebDriverWait(driver, waitSeconds);
+                        WebElement showTextArea = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[@class='link-switch-secondary']")));
+                        showTextArea.click();
+                    } catch (NoSuchElementException ignored) {
+                        System.out.print("-");
+                    }
+                }
+
+                if((driver.findElements(By.xpath("//input[contains(@value,'Отправить отклик')]")).size() > 0)){
+
+                    // cover letter
+                    if((driver.findElements(By.xpath("//textarea[@name='letter']")).size() > 0)) {
+                        try {
+                            Set<String> handles = driver.getWindowHandles();//To handle multiple windows
+                            String firstWinHandle = driver.getWindowHandle();
+                            handles.remove(firstWinHandle);
+                                WebElement coverLetterArea = driver.findElement(By.xpath("//textarea[@name='letter']"));
+                                coverLetterArea.sendKeys("Добрый день!");
+                        } catch (NoSuchElementException ignored) {
+                            System.out.print("-");
+                        }
+                    }
+
+                    //apply
+                    if (driver.findElements(By.xpath("//input[contains(@value,'Отправить отклик')]")).size() > 0) {
+                        // submit click
+                        try {
+                            WebDriverWait wait = new WebDriverWait(driver, waitSeconds);
+                            WebElement submitClickLnk = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[contains(@value,'Отправить отклик')]")));
+                            submitClickLnk.click();
+                        } catch (NoSuchElementException ignored) {
+                            System.out.print("нет элемента");
+                        } catch (Exception e) {
+                            System.out.print("something wrong");
+                        }
+                    }
+
+                    //apply 2-nd time
+                    if (driver.findElements(By.xpath("//input[contains(@value,'Отправить отклик')]")).size() > 0) {
+                        // submit click
+                        try {
+                            WebDriverWait wait = new WebDriverWait(driver, waitSeconds);
+                            WebElement submitClickLnk = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[contains(@value,'Отправить отклик')]")));
+                            submitClickLnk.click();
+                        } catch (NoSuchElementException ignored) {
+                            System.out.print("нет элемента");
+                        } catch (Exception e) {
+                            System.out.print("something wrong");
+                        }
+                    }
+                }
+            }
         }
-
-        // apply from anywhere
-        try {
-            WebDriverWait wait = new WebDriverWait(driver, waitSeconds);
-            WebElement applyFromAnywhereLnk = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[@data-qa='vacancy-response-link-force']")));
-            applyFromAnywhereLnk.click();
-        } catch (NoSuchElementException ignored) {
-            System.out.print("-");
-        }
-
-        // Write a cover letter link click
-        try {
-            WebDriverWait wait = new WebDriverWait(driver, waitSeconds);
-            WebElement writeCoverLetterLnk = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[@class='link-switch-secondary']")));
-            writeCoverLetterLnk.click();
-        } catch (NoSuchElementException ignored) {
-            System.out.print("-");
-        }
-
-        // Write a cover letter text pole click
-        // send cover letter
-        try {
-            WebDriverWait wait = new WebDriverWait(driver, waitSeconds);
-            WebElement writeCoverLetterLnk = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//textarea[@maxlength='10000']")));
-            writeCoverLetterLnk.click();
-
-            writeCoverLetterLnk.sendKeys("Добрый день! \n" +
-                    "Меня заинтересовала Ваша вакансия! \n" +
-                    "Сейчас рассматриваю предложения с удаленной работой.\n" +
-                    "Возможна ли удаленная работа или начать удаленно?\n" +
-                    "С уважением, Александр.\n" +
-                    "\n" +
-                    "LinkedIn: https://www.linkedin.com/in/alexandroid1\n" +
-                    "Locations: Donetsk, Ukraine\n" +
-                    "Phone: 050-426-31-93\n" +
-                    "Skype: alexandr_pavlov_ukraine\n" +
-                    "E-mail: avpavlov108@gmail.com\n" +
-                    "avpavlov96@gmail.com");
-
-        } catch (NoSuchElementException ignored) {
-            System.out.print("-");
-        }
-
-
-        // submit click
-        try {
-            WebDriverWait wait = new WebDriverWait(driver, waitSeconds);
-            WebElement submitClickLnk = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@type='submit']")));
-            submitClickLnk.click();
-        } catch (NoSuchElementException ignored) {
-            System.out.print("-");
-        }
-
-
     }
 }
