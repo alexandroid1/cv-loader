181df149e887e2feebbeb77d4a21966ea084b1a5
Oleksandr <avpavlov96@gmail.com>
2017-01-08 18:49:31 +0300

    synchronization needed

--- src/main/java/HHRunner.java
+++ src/main/java/HHRunner.java
@@ -1,42 +1,26 @@
-import org.openqa.selenium.WebDriver;
-import ua.alexandroid1.oleksandr.settings.TimeSetter;
-import ua.alexandroid1.oleksandr.pages.CVApplyer;
-import ua.alexandroid1.oleksandr.pages.CVLoader;
+import ua.alexandroid1.oleksandr.hunter.HHfinder;
 
-import java.util.*;
-
-public class HHRunner extends TimeSetter {
+public class HHRunner extends HHfinder {
 
     private static final String[] HHLANGS = { "ru", "ua" };
 
     public static void main(String[] args) {
 
         for (int i = 0; i < HHLANGS.length; i++) {
-            System.out.println("Language = " + HHLANGS[i] + " Loaded  ");
-
-            Properties prop = getProperties(HHLANGS[i]);
-            List<String> appliedList = new ArrayList<>();
-            fileToList(appliedList, prop.getProperty("appliedFilePath"));
-            WebDriver driver = getWebDriver(prop);
-            int waitSeconds = getWaitSeconds();
-
-            CVLoader cvLoader = new CVLoader(driver);
-            cvLoader
-                    .setSearchURL(prop.getProperty("searchURL"))
-                    .setSearchKeyWord(prop.getProperty("searchKeyWord"))
-                    .searchByKeyWord(waitSeconds);
-
-            while (cvLoader.getNextPage(waitSeconds)) {
-                ArrayList<String> getCvIds = cvLoader.getCvIds(waitSeconds);
-
-                int langNum = i;
-                getCvIds.forEach((cvId) -> {
-                    if(! appliedList.contains(cvId) ){
-                        CVApplyer cvApplyer = new CVApplyer(driver, cvId);
-                        cvApplyer.applyForCV(appliedList, waitSeconds, HHLANGS[langNum]);
-                    }
-                });
-            }
+            String currentLang = HHLANGS[i];
+            searchByDomain(currentLang);
         }
+
+        // TODO cuncurrency
+/*
+        Thread HHThreadRU = new Thread(() -> searchByDomain("ru"));
+        Thread HHThreadUA = new Thread(() -> searchByDomain("ua"));
+
+
+        HHThreadRU.start();
+        HHThreadUA.start();
+        */
+
     }
+
 }
---
+++ src/main/java/ua/alexandroid1/oleksandr/hunter/HHfinder.java
@@ -0,0 +1,42 @@
+package ua.alexandroid1.oleksandr.hunter;
+
+import org.openqa.selenium.WebDriver;
+import ua.alexandroid1.oleksandr.pages.CVApplyer;
+import ua.alexandroid1.oleksandr.pages.CVLoader;
+import ua.alexandroid1.oleksandr.settings.TimeSetter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+/**
+ * Created by Oleksandr on 08.01.2017.
+ */
+public class HHfinder extends TimeSetter {
+    protected static void searchByDomain(String currentLang) {
+        System.out.println("Language = " + currentLang + " Loaded  ");
+
+        Properties prop = getProperties(currentLang);
+        List<String> appliedList = new ArrayList<>();
+        fileToList(appliedList, prop.getProperty("appliedFilePath"));
+        WebDriver driver = getWebDriver(prop);
+        int waitSeconds = getWaitSeconds();
+
+        CVLoader cvLoader = new CVLoader(driver);
+        cvLoader
+                .setSearchURL(prop.getProperty("searchURL"))
+                .setSearchKeyWord(prop.getProperty("searchKeyWord"))
+                .searchByKeyWord(waitSeconds);
+
+        while (cvLoader.getNextPage(waitSeconds)) {
+            ArrayList<String> getCvIds = cvLoader.getCvIds(waitSeconds);
+
+            getCvIds.forEach((cvId) -> {
+                if(! appliedList.contains(cvId) ){
+                    CVApplyer cvApplyer = new CVApplyer(driver, cvId);
+                    cvApplyer.applyForCV(appliedList, waitSeconds, currentLang);
+                }
+            });
+        }
+    }
+}
