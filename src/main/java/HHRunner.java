84a5abee25862d8b5cc5895f955b3f90674c1270
Oleksandr <avpavlov96@gmail.com>
2017-01-08 19:25:55 +0300

    works for ua

--- src/main/java/HHRunner.java
+++ src/main/java/HHRunner.java
@@ -1,21 +1,10 @@
 import ua.alexandroid1.oleksandr.hunter.HHfinder;
 
 public class HHRunner extends HHfinder {
-
-    private static final String[] HHLANGS = { "ru", "ua" };
-
     public static void main(String[] args) {
 
-/*        for (int i = 0; i < HHLANGS.length; i++) {
-            String currentLang = HHLANGS[i];
-            searchByDomain(currentLang);
-        }*/
-
-        // TODO cuncurrency
-        /*
-        Thread HHThreadRU = new Thread(() -> searchByDomain("ru"));
-        HHThreadRU.start();
-        */
+/*        Thread HHThreadRU = new Thread(() -> searchByDomain("ru"));
+        HHThreadRU.start();*/
 
         Thread HHThreadUA = new Thread(() -> searchByDomain("ua"));
         HHThreadUA.start();
