7736a085a6fc87b274f9158ab731fb4e327b1890
Oleksandr <avpavlov96@gmail.com>
2017-01-09 19:14:34 +0300

    all works

--- src/main/java/HHRunner.java
+++ src/main/java/HHRunner.java
@@ -3,14 +3,9 @@ import ua.alexandroid1.oleksandr.hunter.HHfinder;
 public class HHRunner extends HHfinder {
     public static void main(String[] args) {
 
+        searchByDomain("ru");
+        //searchByDomain("ua");
 
-        // TODO synchronization "ru" and "ua"
-
-        Thread HHThreadRU = new Thread(() -> searchByDomain("ru"));
-        HHThreadRU.start();
-
-/*        Thread HHThreadUA = new Thread(() -> searchByDomain("ua"));
-        HHThreadUA.start();*/
     }
 
 }
