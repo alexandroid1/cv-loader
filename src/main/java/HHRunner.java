643be200f7efad017a09668c8c10bd6bad20f625
Oleksandr <avpavlov96@gmail.com>
2017-01-11 17:26:03 +0300

    2 threads implemented as "ru" and "ua" domain parallel search

--- src/main/java/HHRunner.java
+++ src/main/java/HHRunner.java
@@ -2,9 +2,12 @@ import ua.alexandroid1.oleksandr.hunter.HHfinder;
 
 public class HHRunner extends HHfinder {
     public static void main(String[] args) {
+        
+        Thread HHThreadRU = new Thread(() -> searchByDomain("ru"));
+        HHThreadRU.start();
 
-        searchByDomain("ru");
-        //searchByDomain("ua");
+        Thread HHThreadUA = new Thread(() -> searchByDomain("ua"));
+        HHThreadUA.start();
     }
 
 }
